df13db5dafa0a4c5be70e87f897cd7b90f33a0cb
Alexanne BRUNOLD <alexannebru@gmail.com>
2020-09-13 15:36:04 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <p>Le contenu</p>
+    <button>Click ici</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+</style>
